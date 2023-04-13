baf901539b26c6d72e1ffa3909b2148006cf4254
Tamilarasan <115761431+Tamilarasan3696@users.noreply.github.com>
2023-04-13 16:46:45 +0545

    Update MemberUi.js

--- src/MemberUi.js
+++ src/MemberUi.js
@@ -1,7 +1,6 @@
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import ListGroup from 'react-bootstrap/ListGroup';
-import InfoIcon from '@mui/icons-material/Info';
 import EditIcon from '@mui/icons-material/Edit';
 import DeleteIcon from '@mui/icons-material/Delete';
 import Tooltip from "@mui/material/Tooltip";
@@ -11,10 +10,10 @@ import ArrowBackIosIcon from '@mui/icons-material/ArrowBack';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import axios from 'axios';
 
-function MemberUi() {
+function Member() {
     const [detail, setDetail] = useState([]);
     const getDetail = () => {
-        axios.get("https://63da371319fffcd620c36c33.mockapi.io/teachers")
+        axios.get("https://6437ab94894c9029e8c2db12.mockapi.io/users")
         .then((res)=>setDetail(res.data))
         .catch((err)=>console.log(err));
     }
@@ -28,7 +27,7 @@ function MemberUi() {
             
         <h1> <Tooltip title="Go Back" arrow><Button variant="contained" color="success" onClick={() => navigate(-1)}>
                 <ArrowBackIosIcon />
-            </Button></Tooltip>List of Members 👩🏻‍🦱👨🏻 </h1>
+            </Button></Tooltip>List of Members  </h1>
 
 
             <div class="col-md-12" style={{ justifyItems: 'center' }}>
@@ -36,22 +35,22 @@ function MemberUi() {
                     <container>
                         <row>
                             <Card key={index} style={{ width: '18rem', display: 'inline-flex', marginLeft: '50px', marginTop: '50px' }}>
-                                <Card.Img variant="top" src={object.avatar} />
+                                <Card.Img variant="top" src={object.poster} />
                                 <ListGroup className="list-group-flush">
                                     <ListGroup.Item>Id No : {object.id}</ListGroup.Item>
-                                    <ListGroup.Item>Name : {object.Name}</ListGroup.Item>
-                                    <ListGroup.Item>Age : {object.Age}</ListGroup.Item>
-                                    <ListGroup.Item>Email : {object.Email}</ListGroup.Item>
-                                    <ListGroup.Item>Address : {object.Address}</ListGroup.Item>
-                                    <ListGroup.Item>Contact Number : {object.ContactNumber}</ListGroup.Item>
+                                    <ListGroup.Item>Name : {object.name}</ListGroup.Item>
+                                   
+                                    <ListGroup.Item>Email : {object.email}</ListGroup.Item>
+                                   
+                                    <ListGroup.Item>Contact Number : {object.contact}</ListGroup.Item>
                                 </ListGroup>
                                 <Card.Body>
-                                    <Tooltip title="Information" arrow><Button variant="primary"><InfoIcon onClick={() => navigate(`/memberdetail/${object.id}`)} /></Button></Tooltip>&nbsp;&nbsp;
+                                    
 
-                                    <Tooltip title="Edit" arrow><Button variant="success"><EditIcon onClick={() => navigate(`/editmember/${object.id}`)} /></Button></Tooltip>&nbsp;&nbsp;
+                                    <Tooltip title="Edit" arrow><Button variant="success"><EditIcon onClick={() => navigate(`/user/edit/${object.id}`)} /></Button></Tooltip>&nbsp;&nbsp;
 
                                     <Tooltip title="delete" arrow><Button variant="danger" onClick={() => {
-                                        axios.delete(`https://63da371319fffcd620c36c33.mockapi.io/teachers/${object.id}`)
+                                        axios.delete(`https://6437ab94894c9029e8c2db12.mockapi.io/users/${object.id}`)
                                             .then(() => getDetail());
                                     }}
                                     >
@@ -65,4 +64,4 @@ function MemberUi() {
         </div>
     );
 }
-export default MemberUi;
+export default Member;
